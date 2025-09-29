c2b684ca5b994764894cee658b1acfb3e4a0f30a
Simon Palmer <sipalmer123@gmail.com>
2025-09-29 15:22:51 +0100

    Adjustments

--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using UserManagement.Models;
 using UserManagement.Services.Domain.Interfaces;
 using UserManagement.Web.Models.Users;
